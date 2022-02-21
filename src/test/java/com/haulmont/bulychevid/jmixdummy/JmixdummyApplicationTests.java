d001fc358ef19f61ed1b34be5fc576d6c08c8d21
Ivan Bulychev <i.bulychev@haulmont.com>
2022-02-21 13:09:23 +0400

    Initial commit

---
+++ src/test/java/com/haulmont/bulychevid/jmixdummy/JmixdummyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haulmont.bulychevid.jmixdummy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmixdummyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
